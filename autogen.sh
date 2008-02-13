03a59b39d415366cb7068886f103db8fa6ef6aac
Dan Nicholson <dbn.lists@gmail.com>
2008-02-13 11:52:18 -0800

    More descriptive autogen output for missing autotools
    In the case that autoreconf is missing, we can prompt the user which
    packages will be needed (autoconf and automake). Otherwise, we'll just
    have to rely on the error output from autoreconf, which should be
    sufficiently useful.

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,13 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd "$srcdir"
 
+if ! type autoreconf >/dev/null 2>&1; then
+	echo "**Error**: Missing \`autoreconf' program." >&2
+	echo "You will need the autoconf and automake packages." >&2
+	echo "You can download them from ftp://ftp.gnu.org/pub/gnu/." >&2
+	exit 1
+fi
+
 autoreconf -v --install || exit 1
 cd "$ORIGDIR" || exit $?
 
